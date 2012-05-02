c4df24e41b806f7eceff23179125262e049b5fc2
Jared A. Scheel <jared@jaredscheel.com>
2012-05-02 18:38:24 -0500

    updated site @ 2012-05-02 18:38:24

--- assets/themes/twitter/bootstrap/js/main.js
+++ assets/themes/twitter/bootstrap/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  prettyPrint();
   // directly taken from bootstrap 
   // fix sub nav on scroll
   var $win = $(window)
@@ -25,4 +26,19 @@ $(document).ready(function(){
       $nav.removeClass('subnav-fixed')
     }
   }
+
+  $('body').append('<div class="modal hide fade" id="poppop-modal"><div class="modal-header"><button class="close" data-dismiss="modal">×</button><h3>Pop, Pop!</h3></div><div class="modal-body"></div><div class="modal-footer"><a href="#" class="btn btn-primary" data-dismiss="modal">Get Back to Work</a></div></div>');
+  $('#poppop-modal').modal({show: false});
+  $('#poppop-modal').on('hidden', function(){
+    $('#poppop-modal .modal-body iframe').remove();
+  });
+  var kkeys = [], kcode = "38,38,40,40,37,39,37,39,66,65";
+  $(document).keydown(function(e) {
+    kkeys.push( e.keyCode );
+    if ( kkeys.toString().indexOf( kcode ) >= 0 ){
+      kkeys = [];
+      $('#poppop-modal .modal-body').append('<iframe width="530" height="299" src="http://www.youtube.com/embed/q-_4mcYsQdE?fs=1&autoplay=1&rel=0" frameborder="0" allowfullscreen></iframe>')
+      $('#poppop-modal').modal('show');
+    }
+  });
 });
